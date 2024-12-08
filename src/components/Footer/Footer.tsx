79904d967bd78095b5d8dc2247531e436ee96443
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-12-08 11:13:45 +0600

    Footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 py-8">
+      <div className="container mx-auto text-center">
+        <p className="text-gray-400 text-sm">
+          © 2024 Futurista. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
